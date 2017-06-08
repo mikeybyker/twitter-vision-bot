91503e1b0e7ed7478278d16ba9fa23443dbca791
Mike <mikeybyker@gmail.com>
2017-06-08 10:41:10 +0100

    Cred 7

--- vision.js
+++ vision.js
@@ -10,15 +10,17 @@
   }
 });*/
 // https://stackoverflow.com/questions/42937145/google-vision-api-authentication-on-heroku
-// const vision = require('@google-cloud/vision')({
-//   projectId: 'tweet-bot-1',
-//   credentials: process.env.VISION_KEYFILE_JSON,
-//   VISION_KEYFILE_JSON: process.env.VISION_KEYFILE_JSON
-// });
-
 const vision = require('@google-cloud/vision')({
-  projectId: 'tweet-bot-1'
+  projectId: 'tweet-bot-1',
+  credentials: process.env.VISION_KEYFILE_JSON,
+  VISION_KEYFILE_JSON: process.env.VISION_KEYFILE_JSON
 });
+
+// Could not load the default credentials
+// So need to specify somewhere...
+// const vision = require('@google-cloud/vision')({
+//   projectId: 'tweet-bot-1'
+// });
 const promisify = require('es6-promisify');
 
 const faceEmotions = (face) => ['joy', 'anger', 'sorrow', 'surprise'].filter(emotion => face[emotion]);
